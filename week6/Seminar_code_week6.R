9e4f4aa812d34695b590da07a1be5a56138c420e
George Athanasopoulos <george.athanasopoulos@monash.edu>
2024-04-10 19:44:33 +1000

    Completed Week 6 materials

---
+++ week6/Seminar_code_week6.R
@@ -0,0 +1,121 @@
+library(fpp3)
+
+## Example: Australian holiday tourism ------
+
+holidays <- tourism |>
+  filter(State %in% c("Victoria", "Queensland")) |> 
+  filter(Purpose == "Holiday") |>
+  as_tibble() |>
+  summarise(Trips = sum(Trips), .by = c("State", "Quarter")) |>
+  as_tsibble(index = Quarter, key = State)
+
+holidays |> autoplot()
+
+fit <- holidays |>
+  model(
+    additive = ETS(Trips ~ error("A") + trend("A") + season("A")),
+    multiplicative = ETS(Trips ~ error("M") + trend("A") + season("M")),
+    auto = ETS(Trips)
+  )
+
+fit |>
+  filter(State=="Victoria") |> 
+  select(multiplicative) |>
+  report()
+
+components(fit) |> 
+  filter(State=="Victoria") |> 
+  autoplot()
+
+fit |>
+  filter(State=="Victoria") |> 
+  select(multiplicative) |>
+  components() |>
+  autoplot()
+  # Comment on the model specification and the states
+
+fit |>
+  filter(State=="Victoria") |> 
+  select(additive) |> augment()
+
+fit |>
+  filter(State=="Victoria") |> 
+  select(multiplicative) |> 
+  augment()
+
+fit |> 
+  filter(State=="Victoria") |> 
+  select(multiplicative) |> 
+  residuals()
+
+fit |>
+  filter(State=="Victoria") |> 
+  select(multiplicative) |>
+  gg_tsresiduals()
+
+fit |> 
+  filter(State=="Victoria") |> 
+  select(multiplicative) |> 
+  residuals(type = "response")
+
+fit
+fit |> glance()
+
+fc <- fit |> forecast()
+
+fc |>
+  autoplot(holidays) + 
+  labs(Title="Overnight trips (thousands)")
+
+fc |> 
+  filter(State=="Victoria", !.model=="auto") |> 
+  autoplot(holidays) + 
+  labs(Title="Overnight trips (thousands)")+
+  facet_wrap(vars(.model), ncol = 1)
+
+fc |> 
+  filter(State=="Queensland", !.model=="auto") |> 
+  autoplot(holidays) + 
+  labs(Title="Overnight trips (thousands)")+
+  facet_wrap(vars(.model), ncol = 1)
+
+
+## H02 ------------------------------------
+
+h02 <- PBS |>
+  filter(ATC2 == "H02") |>
+  summarise(Cost = sum(Cost))
+
+h02 |>
+  autoplot(Cost)
+
+h02 |>
+  model(ETS(Cost)) |>
+  report()
+# Comment on the damped trend
+
+h02 |>
+  model(ETS(Cost ~ error("A") + trend("A") + season("A"))) |>
+  report()
+
+h02 |>
+  model(ETS(Cost)) |>
+  forecast() |>
+  autoplot(h02)
+
+
+# Example of STL + ETS
+
+stl_fit <- h02 |>
+  model(
+    decomposition_model(
+      STL(Cost), 
+      ETS(season_adjust),
+      SNAIVE(season_year)
+    )
+  )
+stl_fit |> report()
+
+stl_fit |>
+  forecast(h = 24) |>
+  autoplot(h02)
--- week6/activities.md
+++ week6/activities.md
@@ -1,5 +1,18 @@
+1. Use the tsibble created from `tourism` for holiday travel in Victoria and Queensland. Plot the series to remind yourself what these look like.
 
-Find an ETS model for the Gas data from `aus_production` and forecast the next few years.
+2. Use the `ETS()` function to fit models with additive and multiplicative errors to both series. Also let `ETS()` auto select models. Explore the fitted models and their residuals. 
 
-  * Why is multiplicative seasonality necessary here?
-  * Experiment with making the trend damped. Does it improve the forecasts?
+3. Generate forecasts from the fitted models. Why is the multiplicative model needed for Victoria? 
+
+4. Generate the `h02` series from the `PBS` tsibble we explored earlier using the code below.
+
+    ```r
+    h02 <- PBS |>
+      filter(ATC2 == "H02") |>
+      summarise(Cost = sum(Cost))
+    ```
+5. Find an `ETS` model and study it. Why has a damped trend been selected? 
+
+6. Generate forecasts for the next few years. 
+
+7. Combine `STL` decomposition with `ETS` to forecast the `h02` series.
